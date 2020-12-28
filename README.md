07655588d1733c6fba3d18ffb4e15f54ba80e3a8
Andre Weissflog <floooh@gmail.com>
2020-12-28 14:54:22 +0100

    clean up build script and add build instructions to readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pacman.zig
 Like https://github.com/floooh/pacman.c, but in Zig (WIP)
+
+## Build and Run
+
+>NOTE Currently only Linux is supported (Windows coming soon, Mac might take while)
+
+```
+git clone https://github.com/floooh/pacman.zig
+cd pacman.zig
+zig build run
+```
+
+On Linux, you need to install the usual packages for GL-, X11- and ALSA-development.
+
--- build.zig
+++ build.zig
@@ -1,19 +1,28 @@
+const bld = @import("std").build;
 const Builder = @import("std").build.Builder;
+const LibExeObjStep = @import("std").build.LibExeObjStep;
 const builtin = @import("std").builtin;
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const e = b.addExecutable("pacman", "src/pacman.zig");
+// build sokol into its own static link library
+fn buildSokol(b: *Builder) *LibExeObjStep {
+    const l = b.addStaticLibrary("sokol", null);
+    l.addCSourceFile("src/sokol/sokol.c", &[_][]const u8{});
+    l.setBuildMode(b.standardReleaseOptions());
+    l.linkSystemLibrary("c");
     if (builtin.os.tag == .linux) {
-        e.linkSystemLibrary("X11");
-        e.linkSystemLibrary("Xi");
-        e.linkSystemLibrary("Xcursor");
-        e.linkSystemLibrary("GL");
+        l.linkSystemLibrary("X11");
+        l.linkSystemLibrary("Xi");
+        l.linkSystemLibrary("Xcursor");
+        l.linkSystemLibrary("GL");
     }
-    e.setBuildMode(mode);
+    return l;
+}
+
+pub fn build(b: *Builder) void {
+    const e = b.addExecutable("pacman", "src/pacman.zig");
+    e.linkLibrary(buildSokol(b));
+    e.setBuildMode(b.standardReleaseOptions());
     e.addPackagePath("sokol", "src/sokol/sokol.zig");
-    e.addCSourceFile("src/sokol/sokol.c", &[_][]const u8{});
-    e.linkSystemLibrary("c");
     e.install();
     b.step("run", "Run pacman").dependOn(&e.run().step);
 }
