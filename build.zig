41dd8c28cc02eacb112ee9e455a18772c65583ca
Andre Weissflog <floooh@gmail.com>
2021-01-02 14:26:08 +0100

    change macOS build to not require system clang

--- build.zig
+++ build.zig
@@ -1,23 +1,25 @@
 const Builder = @import("std").build.Builder;
 const LibExeObjStep = @import("std").build.LibExeObjStep;
 const builtin = @import("std").builtin;
+const mem = @import("std").mem;
 
-fn addSokol(b: *Builder, exe: *LibExeObjStep) void {
+// macOS helper function to add SDK search paths 
+fn macosAddSdkDirs(b: *Builder, step: *LibExeObjStep) !void {
+    var sdk_dir = try b.exec(&[_][]const u8 { "xcrun", "--show-sdk-path" });
+    const newline_index = mem.lastIndexOf(u8, sdk_dir, "\n");
+    if (newline_index) |idx| {
+        sdk_dir = sdk_dir[0..idx];
+    }
+    const framework_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/System/Library/Frameworks" });
+    const usrinclude_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/usr/include"});
+    step.addFrameworkDir(framework_dir);
+    step.addIncludeDir(usrinclude_dir);
+}
+
+fn addSokol(b: *Builder, exe: *LibExeObjStep) !void {
     if (builtin.os.tag == .macos) {
-        //
-        // On macOS, can't use Zig's C compiler to build the Sokol headers,
-        // because those must be compiled with Objective-C.
-        //
-        // Building on macOS currently also required setting an env variable
-        // to use the system linker instead of Zig's:
-        //
-        // export ZIG_SYSTEM_LINKER_HACK=1
-        //
-        // FIXME: use optimization / debug compile options depending on zig build options
-        //
-        const clangCmd = &[_][]const u8 { "clang", "-x", "objective-c", "-c", "src/sokol/sokol.c", "-Os", "-o", "zig-cache/sokol.o"};
-        exe.step.dependOn(&b.addSystemCommand(clangCmd).step);
-        exe.addObjectFile("zig-cache/sokol.o");
+        try macosAddSdkDirs(b, exe);
+        exe.addCSourceFile("src/sokol/sokol.c", &[_][]const u8 { "-ObjC" });
         exe.linkFramework("MetalKit");
         exe.linkFramework("Metal");
         exe.linkFramework("Cocoa");
@@ -25,7 +27,6 @@ fn addSokol(b: *Builder, exe: *LibExeObjStep) void {
         exe.linkFramework("AudioToolbox");
     }
     else {
-        // Windows and Linux can use Zig's C compiler and don't need special linker hacks
         exe.addCSourceFile("src/sokol/sokol.c", &[_][]const u8{});
         exe.linkSystemLibrary("c");
         if (builtin.os.tag == .linux) {
@@ -40,7 +41,7 @@ fn addSokol(b: *Builder, exe: *LibExeObjStep) void {
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("pacman", "src/pacman.zig");
-    addSokol(b, exe);
+    addSokol(b, exe) catch unreachable;
     exe.setBuildMode(b.standardReleaseOptions());
     exe.addPackagePath("sokol", "src/sokol/sokol.zig");
     exe.install();
