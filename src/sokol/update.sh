24dca733989153d422aa989d13675ab83bb02e38
Andre Weissflog <floooh@gmail.com>
2021-02-26 17:03:26 +0100

    update sokol-zig bindings

---
+++ src/sokol/update.sh
@@ -0,0 +1,10 @@
+cp ../../../sokol-zig/src/sokol/app_gfx_glue.zig .
+cp ../../../sokol-zig/src/sokol/app.zig .
+cp ../../../sokol-zig/src/sokol/audio.zig .
+cp ../../../sokol-zig/src/sokol/gfx.zig .
+cp ../../../sokol-zig/src/sokol/time.zig .
+cp ../../../sokol-zig/src/sokol/c/sokol_app.h .
+cp ../../../sokol-zig/src/sokol/c/sokol_audio.h .
+cp ../../../sokol-zig/src/sokol/c/sokol_gfx.h .
+cp ../../../sokol-zig/src/sokol/c/sokol_time.h .
+
