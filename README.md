728745309f5ced09c65d31e1f28b417a88f6feb6
Andre Weissflog <floooh@gmail.com>
2021-06-06 14:11:57 +0200

    updated for zig 0.8.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pacman.zig
-Like https://github.com/floooh/pacman.c, but in Zig
+Like https://github.com/floooh/pacman.c, but in Zig.
 
 Zig bindings for the sokol headers are here: https://github.com/floooh/sokol-zig
 
@@ -7,21 +7,13 @@ Zig bindings for the sokol headers are here: https://github.com/floooh/sokol-zig
 
 Zig installation: https://github.com/ziglang/zig/wiki/Install-Zig-from-a-Package-Manager
 
-On **macOS** only, a 'system linker hack' is currently needed which requires setting
-an environment variable in the shell session where 'zig build' will run:
+Tested with Zig 0.8.0
 
-```
-# macOS only:
-> export ZIG_SYSTEM_LINKER_HACK=1
-```
-
-From here on it's the same procedure on macOS, Windows and Linux:
 ```
 > git clone https://github.com/floooh/pacman.zig
 > cd pacman.zig
 > zig build run
 ```
-Tested with zig version 0.7.1+
 
 On Windows, rendering is done through D3D11, on Linux through OpenGL and
 on macOS through Metal.
--- build.zig
+++ build.zig
@@ -1,20 +1,10 @@
 const bld = @import("std").build;
 const mem = @import("std").mem;
-const zig = @import("std").zig;
-
-// macOS helper function to add SDK search paths 
-fn macosAddSdkDirs(b: *bld.Builder, step: *bld.LibExeObjStep) !void {
-    const sdk_dir = try zig.system.getSDKPath(b.allocator);
-    const framework_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/System/Library/Frameworks" });
-    const usrinclude_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/usr/include"});
-    step.addFrameworkDir(framework_dir);
-    step.addIncludeDir(usrinclude_dir);
-}
 
 fn addSokol(b: *bld.Builder, exe: *bld.LibExeObjStep) !void {
     exe.linkLibC();
     if (exe.target.isDarwin()) {
-        try macosAddSdkDirs(b, exe);
+        b.env_map.put("ZIG_SYSTEM_LINKER_HACK", "1") catch unreachable;
         exe.addCSourceFile("src/sokol/sokol.c", &[_][]const u8 { "-ObjC" });
         exe.linkFramework("MetalKit");
         exe.linkFramework("Metal");
