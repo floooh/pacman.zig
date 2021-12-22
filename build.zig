6f0b983de81659282fbf3bc63a1e36c9b40b4a18
Andre Weissflog <floooh@gmail.com>
2021-12-22 14:33:05 +0100

    cleanup build.zig and add iOS cross-compilation support

--- build.zig
+++ build.zig
@@ -1,62 +1,88 @@
 const std = @import("std");
-const bld = @import("std").build;
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
+const CrossTarget = std.zig.CrossTarget;
+const Mode = std.builtin.Mode;
+const builtin = @import("builtin");
 
-pub fn build(b: *bld.Builder) void {
-    const target = b.standardTargetOptions(.{});
+pub fn build(b: *Builder) void {
     const exe = b.addExecutable("pacman", "src/pacman.zig");
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+    const cross_compiling = (target.os_tag != null) or !builtin.target.isDarwin();
+
     exe.setTarget(target);
-    addSokol(exe);
-    exe.setBuildMode(b.standardReleaseOptions());
+    exe.setBuildMode(mode);
+    exe.linkLibrary(buildSokol(b, target, mode, cross_compiling, ""));
     exe.addPackagePath("sokol", "src/sokol/sokol.zig");
+    if (cross_compiling) {
+        exe.addLibPath("/usr/lib");
+        exe.addFrameworkDir("/System/Library/Frameworks");
+    }
     exe.install();
+
     b.step("run", "Run pacman").dependOn(&exe.run().step);
-    const install_path = std.fmt.allocPrint(b.allocator, "{s}/bin/pacman", .{b.install_path}) catch unreachable;
-    std.debug.print("install_path: {s}\n", .{ install_path });
-    defer b.allocator.free(install_path);
-    b.installFile(install_path, "bin/Pacman.app/pacman");
-    b.installFile("Info.plist", "bin/Pacman.app/Info.plist");
+    
+    if (target.getOsTag() == .ios) {
+        const install_path = std.fmt.allocPrint(b.allocator, "{s}/bin/pacman", .{b.install_path}) catch unreachable;
+        defer b.allocator.free(install_path);
+        b.installFile(install_path, "bin/Pacman.app/pacman");
+        b.installFile("src/ios/Info.plist", "bin/Pacman.app/Info.plist");
+    }
 }
 
-fn addSokol(exe: *bld.LibExeObjStep) void {
-    exe.linkLibC();
-    if (exe.target.isDarwin()) {
-//        if (!@import("builtin").target.isDarwin()) {
-            // NOTE: this is for cross-compilation support
-            //const sdk_path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.2.sdk";
-            const sdk_path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.2.sdk";
-            exe.addSystemIncludeDir(sdk_path ++ "/usr/include");
-            exe.addLibPath(sdk_path ++ "/usr/lib");
-            exe.addFrameworkDir(sdk_path ++ "/System/Library/Frameworks");
-//        }
-        exe.addCSourceFile("src/sokol/sokol.c", &.{ "-ObjC" });
-        exe.linkFramework("MetalKit");
-        exe.linkFramework("Metal");
-        exe.linkFramework("UIKit");
-        exe.linkFramework("AudioToolbox");
-        exe.linkFramework("CoreFoundation");
-        exe.linkFramework("Foundation");
-        exe.linkFramework("AVFoundation");
-        exe.linkFramework("FileProvider");
-        exe.linkSystemLibrary("System.B");
-        exe.linkSystemLibrary("objc.A");
-
+fn buildSokol(b: *Builder, target: CrossTarget, mode: Mode, cross_compiling: bool, comptime prefix_path: []const u8) *LibExeObjStep {
+    const lib = b.addStaticLibrary("sokol", null);
+    lib.setTarget(target);
+    lib.setBuildMode(mode);
+    lib.linkLibC();
+    const sokol_path = prefix_path ++ "src/sokol/sokol.c";
+    if (lib.target.isDarwin()) {
+        lib.addCSourceFile(sokol_path, &.{ "-ObjC" });
+        lib.linkFramework("MetalKit");
+        lib.linkFramework("Metal");
+        lib.linkFramework("AudioToolbox");
+        if (target.getOsTag() == .ios) {
+            lib.linkFramework("UIKit");
+            lib.linkFramework("AVFoundation");
+            lib.linkFramework("Foundation");
+        }
+        else {
+            lib.linkFramework("Cocoa");
+            lib.linkFramework("QuartzCore");
+        }
     }
     else {
-        exe.addCSourceFile("src/sokol/sokol.c", &.{});
-        if (exe.target.isLinux()) {
-            exe.linkSystemLibrary("X11");
-            exe.linkSystemLibrary("Xi");
-            exe.linkSystemLibrary("Xcursor");
-            exe.linkSystemLibrary("GL");
-            exe.linkSystemLibrary("asound");
+        lib.addCSourceFile(sokol_path, &.{});
+        if (lib.target.isLinux()) {
+            lib.linkSystemLibrary("X11");
+            lib.linkSystemLibrary("Xi");
+            lib.linkSystemLibrary("Xcursor");
+            lib.linkSystemLibrary("GL");
+            lib.linkSystemLibrary("asound");
         }
-        else if (exe.target.isWindows()) {
-            exe.linkSystemLibrary("kernel32");
-            exe.linkSystemLibrary("user32");
-            exe.linkSystemLibrary("gdi32");
-            exe.linkSystemLibrary("ole32");
-            exe.linkSystemLibrary("d3d11");
-            exe.linkSystemLibrary("dxgi");
+        else if (lib.target.isWindows()) {
+            lib.linkSystemLibrary("kernel32");
+            lib.linkSystemLibrary("user32");
+            lib.linkSystemLibrary("gdi32");
+            lib.linkSystemLibrary("ole32");
+            lib.linkSystemLibrary("d3d11");
+            lib.linkSystemLibrary("dxgi");
         }
     }
+    // setup cross-compilation search paths
+    if (cross_compiling) {
+        if (b.sysroot == null) {
+            std.log.warn("===================================================================================", .{});
+            std.log.warn("You haven't set the path to Apple SDK which may lead to build errors.", .{});
+            std.log.warn("Hint: you can the path to Apple SDK with --sysroot <path> flag like so:", .{});
+            std.log.warn("  zig build --sysroot $(xcrun --sdk iphoneos --show-sdk-path) -Dtarget=aarch64-ios", .{});
+            std.log.warn("or:", .{});
+            std.log.warn("  zig build --sysroot $(xcrun --sdk iphonesimulator --show-sdk-path) -Dtarget=aarch64-ios-simulator", .{});
+            std.log.warn("===================================================================================", .{});
+        }
+        lib.addFrameworkDir("/System/Library/Frameworks");
+        lib.addSystemIncludeDir("/usr/include");
+    }
+    return lib;
 }
