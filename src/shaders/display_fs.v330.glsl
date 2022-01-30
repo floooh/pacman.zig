24c3f6652cda06dcabc565ae6a3ef87fda4303be
Andre Weissflog <floooh@gmail.com>
2022-01-30 17:37:10 +0100

    initial webpage commit

--- src/shaders/display_fs.v330.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-uniform sampler2D tex;
-in vec2 uv;
-out vec4 frag_color;
-void main() {
-    frag_color = texture(tex, uv);
-}
--- src/shaders/display_vs.v330.glsl
+++
@@ -1,8 +0,0 @@
-
-#version 330
-layout(location=0) in vec4 pos;
-out vec2 uv;
-void main() {
-      gl_Position = vec4((pos.xy - 0.5) * 2.0, 0.0, 1.0);
-      uv = pos.xy;
-}
