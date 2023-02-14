133607402ba8f937ae915a9d598589446602dc73
Andre Weissflog <floooh@gmail.com>
2023-02-14 18:58:21 +0100

    remove the SOKOL_LOG macro from sokol.c, no longer needed

--- src/sokol/sokol.c
+++ src/sokol/sokol.c
@@ -4,7 +4,6 @@
 #if defined(_WIN32)
     #define SOKOL_WIN32_FORCE_MAIN
     #define SOKOL_D3D11
-    #define SOKOL_LOG(msg) OutputDebugStringA(msg)
 #elif defined(__APPLE__)
     #define SOKOL_METAL
 #elif defined(__EMSCRIPTEN__)
